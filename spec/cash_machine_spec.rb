3ab7bc52987682af279da4f7ad11c2fd0158319b
Kevin Agoh <kevinagoh@hotmail.fr>
2024-05-06 19:18:47 +0200

    fix: Fixed tests

--- spec/cash_machine_spec.rb
+++ spec/cash_machine_spec.rb
@@ -19,49 +19,48 @@ RSpec.describe CashMachine do
     cash_machine = CashMachine.new
     products_repository = ProductsRepositories.new
     cash_machine_controller = CashMachineController.new(cash_machine, products_repository)
-    product1 = { name: 'Green Tea', price: 3.11, code: 'GR1' }
-    product2 = { name: 'Strawberries', price: 5.00, code: 'SR1' }
-    cash_machine.add(product1)
-    cash_machine.add(product2)
-    expect(cash_machine_controller.compute_total_price).to eq(8.11)
+    green_tea = products_repository.all[0]
+    strawberries = products_repository.all[1]
+    cash_machine.add(green_tea)
+    cash_machine.add(strawberries)
+    expect(cash_machine_controller.compute_total_price(cash_machine.all)).to eq(8.11)
   end
 
   it 'applies a buy one, get one for free discount when you add a second tea to the cart' do
     cash_machine = CashMachine.new
     products_repository = ProductsRepositories.new
     cash_machine_controller = CashMachineController.new(cash_machine, products_repository)
-
-    product1 = { name: 'Green Tea', price: 3.11, code: 'GR1' }
-    cash_machine.add(product1)
-    cash_machine.add(product1)
-    expect(cash_machine_controller.compute_total_price).to eq(3.11)
+    green_tea = products_repository.all[0]
+    cash_machine.add(green_tea)
+    cash_machine.add(green_tea)
+    expect(cash_machine_controller.compute_total_price(cash_machine.all)).to eq(3.11)
   end
 
   it 'reduces prices of strawberries to 4,50€ if there are 3 or more strawberries in the cart' do
     cash_machine = CashMachine.new
     products_repository = ProductsRepositories.new
     cash_machine_controller = CashMachineController.new(cash_machine, products_repository)
-    product1 = { name: 'Green Tea', price: 3.11, code: 'GR1' }
-    product2 = { name: 'Strawberries', price: 5.00, code: 'SR1' }
-    cash_machine.add(product1)
-    cash_machine.add(product2)
-    cash_machine.add(product1)
-    cash_machine.add(product1)
-    expect(cash_machine_controller.compute_total_price).to eq(16.61)
+    green_tea = products_repository.all[0]
+    strawberries = products_repository.all[1]
+    cash_machine.add(strawberries)
+    cash_machine.add(green_tea)
+    cash_machine.add(strawberries)
+    cash_machine.add(strawberries)
+    expect(cash_machine_controller.compute_total_price(cash_machine.all)).to eq(16.61)
   end
 
   it 'reduces prices of strawberries to 2/3 of the original price if there are 3 or more coffees in the cart' do
     cash_machine = CashMachine.new
     products_repository = ProductsRepositories.new
     cash_machine_controller = CashMachineController.new(cash_machine, products_repository)
-    product1 = { name: 'Green Tea', price: 3.11, code: 'GR1' }
-    product2 = { name: 'Strawberries', price: 5.00, code: 'SR1' }
-    product3 = { name: 'Coffee', price: 11.23, code: 'CF1' }
-    cash_machine.add(product1)
-    cash_machine.add(product3)
-    cash_machine.add(product2)
-    cash_machine.add(product3)
-    cash_machine.add(product3)
-    expect(cash_machine_controller.compute_total_price).to eq(30.57)
+    green_tea = products_repository.all[0]
+    strawberries = products_repository.all[1]
+    coffee = products_repository.all[2]
+    cash_machine.add(green_tea)
+    cash_machine.add(coffee)
+    cash_machine.add(strawberries)
+    cash_machine.add(coffee)
+    cash_machine.add(coffee)
+    expect(cash_machine_controller.compute_total_price(cash_machine.all)).to eq(30.57)
   end
 end
